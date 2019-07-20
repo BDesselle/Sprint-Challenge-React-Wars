14cb4e11cb86711fcaa327486706b5d12a26e3bf
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-19 19:48:39 -0500

    Update App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
+import CharCard from './components/CharCard/CharCard';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [data, setData] = React.useState([]);
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  React.useEffect(() => {
+    axios
+      /* GET */
+      .get('https://swapi.co/api/people/')
+      /* THEN */
+      .then(res => {
+        const results = res.data.results;
+        console.log('%c Logging Response Results...', 'color: green; font-weight: bold;');
+        console.table(results);
+        setData(results);
+      })
+      /* CATCH */
+      .catch(err => {
+        console.log('%c Error Caught!', 'color: red; font-weight: bold;');
+        console.log(err);
+      })
+  }, []);
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <h1 className="Header">ReactWars</h1>
+      <div class="container">
+        <CharCard characters={data} />
+      </div>
     </div>
   );
 }
